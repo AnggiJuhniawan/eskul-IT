7f7434ba138e1dab79cbc17890ea214ab0753ce8
Anggi Juhniawan <anggijuhniawan23@gmail.com>
2026-08-22 09:48:04 +0700

    ganti nama folder

--- WK/Latihan1/obj/Debug/net10.0/Latihan1.AssemblyInfo.cs
+++ WK/Latihan1/obj/Debug/net10.0/Latihan1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Latihan1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc878c14237d688caa83b16baa45d8f382cfce95")]
 [assembly: System.Reflection.AssemblyProductAttribute("Latihan1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Latihan1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
