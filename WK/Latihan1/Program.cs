b56935edd6bc015267e04ad2e8fa0d58e23f01b1
Anggi Juhniawan <anggijuhniawan23@gmail.com>
2026-08-22 14:53:43 +0700

    Variable C#

--- WK/Latihan1/Program.cs
+++ WK/Latihan1/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine($"Halo, nama saya {nama}");
+
+// string nama = "Anggi Juhniawan";
+// int umur = 16;
+
+// Console.WriteLine($"Halo, nama saya {nama}");
+// Console.WriteLine("Umur saya adalah" + umur + " tahun");
+
+// Console.WriteLine($"Saya ulang tahun besok sudah umur {umur}");
+
+// INPUT & OUTPUT
+Console.Write("Masukkan Nama : ");
+string nama = Console.ReadLine();
+
+Console.WriteLine($"Namaku adalah {nama}");
--- WK/Latihan1/obj/Debug/net10.0/Latihan1.AssemblyInfo.cs
+++ WK/Latihan1/obj/Debug/net10.0/Latihan1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Latihan1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc878c14237d688caa83b16baa45d8f382cfce95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1862d6b757df59b1ebb34c84cdd56c4a0293d1d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Latihan1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Latihan1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
